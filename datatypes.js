85110a6deded18f8e4f2dc346dc8dfcab27fcc42
Jairo Tabora <veistudent@gmail.com>
2022-12-20 21:23:22 -0500

    update to replicate contents to local storage

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,6 @@
-"use strict";
-let lname = 'myLastName';
-console.log(`in TS, the var lname has been init and assigned: ${lname}`);
-lname = 19;
-console.log(`after we attempted to re-assign the var 'lname', now we get an error`);
+var lname = 'myLastName';
+console.log("in TS, the var lname has been init and assigned: ".concat(lname));
+console.log("\nif we attempted to re-assign the var 'lname', now we get an error");
+console.log("\nHowever, if we re-assing the var lname (which is of type ".concat(typeof lname, ")"));
+lname = 'hahaha';
+console.log("\nNow, we renamed 'lname' to: ".concat(lname));
