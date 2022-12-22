4304cb90392a9263211d0ca8baf5cfb472be406f
JaiTab <67428344+veistudent@users.noreply.github.com>
2022-12-21 21:39:30 -0500

    updated datatypes from hp laptop

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,8 @@
-var lname = 'myLastName';
-console.log("in TS, the var lname has been init and assigned: ".concat(lname));
-console.log("\nif we attempted to re-assign the var 'lname', now we get an error");
-console.log("\nHowever, if we re-assing the var lname (which is of type ".concat(typeof lname, ")"));
-lname = 'hahaha';
-console.log("\nNow, we renamed 'lname' to: ".concat(lname));
+"use strict";
+let lname = 'myLastName';
+console.log(`\n\nin TS, the var lname has been init and assigned: ${lname}`);
+// console.log(`\nif we attempted to re-assign the var 'lname', now we get an error`);
+console.log(`\nHowever, if we re-assing the var lname (which is of type '${typeof lname}') with another string...`);
+lname = 'wolverine';
+lname = lname.charAt(0).toUpperCase() + lname.substring(1).toLowerCase();
+console.log(`\nNow, we renamed 'lname' to: '${lname}' (and we made it proper upper case)`);
--- datatypes.ts
+++ datatypes.ts
@@ -1,7 +1,8 @@
 let lname = 'myLastName';
-console.log(`in TS, the var lname has been init and assigned: ${lname}`);
+console.log(`\n\nin TS, the var lname has been init and assigned: ${lname}`);
 
-console.log(`if we attempted to re-assign the var 'lname', now we get an error`);
-console.log(`However, if we re-assing the var lname (which is of type ${typeof lname})`);
-lname = 'hahaha';
-console.log(`Now, we renamed 'lname' to: ${lname}`);
+// console.log(`\nif we attempted to re-assign the var 'lname', now we get an error`);
+console.log(`\nHowever, if we re-assing the var lname (which is of type '${typeof lname}') with another string...`);
+lname = 'wolverine';
+lname = lname.charAt(0).toUpperCase() + lname.substring(1).toLowerCase();
+console.log(`\nNow, we renamed 'lname' to: '${lname}' (and we made it proper upper case)`);
