f7103727859e072ce036bfcaef8ce89f8ea6334c
Jairo Tabora <veistudent@gmail.com>
2023-02-21 20:53:21 -0500

    updated files up to end of TS portion of video approx 2:35:00

--- angDefaultApp/hotelInventoryApp/src/app/app.component.ts
+++ angDefaultApp/hotelInventoryApp/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  // templateUrl: './app.component.html',
+  template: `<p>\npeeky heyo world!\n\n<h2>inline inside ts file </h2></p>`,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hotelInventoryApp';
--- angDefaultApp/hotelInventoryApp/src/main.ts
+++ angDefaultApp/hotelInventoryApp/src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+// import { environment } from './environment/environment';
 
+// if (environment.production) { enableProdMode();}   //works only if in production mode 
+enableProdMode();                                     // here, we force and assume production mode.
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
